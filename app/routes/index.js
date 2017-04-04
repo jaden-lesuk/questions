ded7a98050681e8ff11610d8758c8c06ed607554
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-04 11:17:35 +0300

    ADDED sweetAlert and delete button
    Added the library sweet Alert and a functional delete button

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,13 @@
 import Ember from 'ember';
 
-var posts = [{
-		author: "Jaden Lesuk",
-		content: "Would it be easier to use Angular or Ember to build a social network",
-		addition: "A.S.A.P plz"
-	},
-	{
-		author: "Bry Onyoni",
-		content: "Wouldn't static types just bring more bugs, looking at you typescript",
-		addition: "Just asking"
-	},
-	{
-		author: "Morris Mburu",
-		content: "Does majestic and stupendous mean the same thing?",
-		addition: "I don't have a dictionary"
-	}
-];
-
 export default Ember.Route.extend({
 	model() {
-		return posts;
+		return this.store.findAll('post');
 	},
+	actions: {
+		deletePost(post) {
+			post.destroyRecord();
+			this.transitionTo('index');
+		}
+	}
 });
