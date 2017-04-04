f1290a1a8361c101999dbf8d90478e66d47a069a
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-04 11:59:22 +0300

    ADDED NEW POST FUNCTIONALITY
    Added the ability to create and save a new post

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	addNewPost: false,
+	actions: {
+		postFormShow() {
+			this.set('addNewPost', true);
+		},
+		savePost1() {
+			var params = {
+				author: this.get('author'),
+				content: this.get('content'),
+				addition: this.get('addition')
+			};
+			this.set('addNewPost', false);
+			this.sendAction('savePost2', params);
+		}
+	}
 });
