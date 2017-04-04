eb44172e58cb2b9864b8958d8dcd8c5741b138eb
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-04 08:27:09 +0300

    ADDED SAMPLE DATA
    Added sample questions to the index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var posts = [{
+		author: "Jaden Lesuk",
+		content: "Would it be easier to use Angular or Ember to build a social network",
+		addition: "A.S.A.P plz"
+	},
+	{
+		author: "Bry Onyoni",
+		content: "Wouldn't static types just bring more bugs, looking at you typescript",
+		addition: "Just asking"
+	},
+	{
+		author: "Morris Mburu",
+		content: "Does majestic and stupendous mean the same thing?",
+		addition: "I don't have a dictionary"
+	}
+];
+
 export default Ember.Route.extend({
+	model() {
+		return posts;
+	},
 });
--- config/environment.js
+++ config/environment.js
@@ -1,50 +1,57 @@
 /* jshint node: true */
 
-module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'questions',
-    environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+module.exports = function (environment) {
+	var ENV = {
+		modulePrefix: 'questions',
+		environment: environment,
+		rootURL: '/',
+		locationType: 'auto',
+		EmberENV: {
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			},
+			EXTEND_PROTOTYPES: {
+				// Prevent Ember Data from overriding Date.parse.
+				Date: false
+			}
+		},
+		firebase: {
+			apiKey: "AIzaSyC7msx47Xd_JVbavqh_TSOg3j1jGmbOrMI",
+			authDomain: "questions-32c32.firebaseapp.com",
+			databaseURL: "https://questions-32c32.firebaseio.com",
+			projectId: "questions-32c32",
+			storageBucket: "questions-32c32.appspot.com",
+			messagingSenderId: "149568987273"
+		},
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		}
+	};
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
+	}
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+	if (environment === 'test') {
+		// Testem prefers this...
+		ENV.locationType = 'none';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+		// keep test console output quieter
+		ENV.APP.LOG_ACTIVE_GENERATION = false;
+		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+		ENV.APP.rootElement = '#ember-testing';
+	}
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+	if (environment === 'production') {
 
-  if (environment === 'production') {
+	}
 
-  }
-
-  return ENV;
+	return ENV;
 };
