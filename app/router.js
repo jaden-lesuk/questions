1a0abf36ffae3fa9eca07609a09d3d58d38446dd
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-07 14:28:33 +0300

    LAID OUT FOUNDATION FOR DYNAMIC ROUTING
    Added a dynamic routing path to the router.js

--- app/router.js
+++ app/router.js
@@ -2,14 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('about-us');
-  this.route('contact');
-  this.route('post');
+Router.map(function () {
+	this.route('about-us');
+	this.route('contact');
+	this.route('post', {
+		path: '/post/:post_id'
+	});
 });
 
 export default Router;
