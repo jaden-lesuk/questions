44b87b6d29c499a07d8be3646a499abb8f6a1add
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-07 12:05:11 +0300

    ADDED UPDATE POST FORM
    Added a form to update a post and passed the action upwards

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	updatePostForm: false,
 	actions: {
 		delete(post) {
 			if (confirm("Are you sure?")) {
 				this.sendAction('deletePost', post);
 			}
+		},
+		update(post, params) {
+
 		}
 	},
 });
--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	updatePostForm: false,
+	actions: {
+		cancel() {
+			this.set("updatePostForm", false);
+		},
+		updatePostForm() {
+			this.set("updatePostForm", true);
+		},
+		update(post) {
+			var params = {
+				author: this.get('author'),
+				content: this.get('content'),
+				addition: this.get('addition')
+			};
+			this.set("updatePostForm", false);
+			this.sendAction('update', post, params);
+		}
+	}
 });
